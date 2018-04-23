9359acb5517a2b4582f39cbd0bbfa2f0580bd852
konradko <konrad@korzel.com>
2018-04-23 15:51:05 +0100

    Add exred privacy and cookies

--- directory_cms_client/export_readiness.py
+++ directory_cms_client/export_readiness.py
@@ -5,11 +5,19 @@ class ExportReadinessClient(BaseAPIClient):
 
     page_types = {
         'terms-and-conditions': 'export_readiness.TermsAndConditionsPage',
+        'privacy-and-cookies': 'export_readiness.PrivacyAndCookiesPage',
     }
 
     def get_terms_and_conditions_page(self, *args, **kwargs):
         return self.lookup_by_page_type(
             page_type=self.page_types['terms-and-conditions'],
+            *args,
+            **kwargs
+        )
+
+    def get_privacy_and_cookies_page(self, *args, **kwargs):
+        return self.lookup_by_page_type(
+            page_type=self.page_types['privacy-and-cookies'],
             *args,
             **kwargs
         )
