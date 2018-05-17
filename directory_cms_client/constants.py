3706d5c655fc53346504ce4a75c6b0311df68a38
richtier <rikatee@gmail.com>
2018-05-17 16:25:18 +0100

    Add ability to list by page type

---
+++ directory_cms_client/constants.py
@@ -0,0 +1,8 @@
+EXPORT_READINESS_TERMS_AND_CONDITIONS_SLUG = 'terms-and-conditions'
+EXPORT_READINESS_PRIVACY_AND_COOKIES_SLUG = 'privacy-and-cookies'
+
+FIND_A_SUPPLIER_INDUSTRY_LANDING_SLUG = 'industries-landing-page'
+FIND_A_SUPPLIER_LANDING_SLUG = 'landing-page'
+FIND_A_SUPPLIER_INDUSTRY_CONTACT_SLUG = 'industry-contact'
+FIND_A_SUPPLIER_INDUSTRY_TYPE = 'find_a_supplier.IndustryPage'
+FIND_A_SUPPLIER_INDUSTRY_ARTICLE_TYPE = 'find_a_supplier.IndustryArticlePage'
