41ec23564805a9d63c4762813e8ee33cdd7a7e4c
Andy Babic <andyjbabic@gmail.com>
2019-04-08 10:46:49 +0100

    Update lookup_by_path() to support passing of language_code

--- directory_cms_client/client.py
+++ directory_cms_client/client.py
@@ -119,10 +119,12 @@ class DirectoryCMSClient(AbstractAPIClient):
         path,
         fields=None,
         draft_token=None,
+        language_code=None,
     ):
         base_params = build_params(
             fields=fields,
             draft_token=draft_token,
+            language_code=language_code,
         )
         return self.get(
             url=self.endpoints['page-by-path'].format(site_id=site_id, path=path), # noqa
